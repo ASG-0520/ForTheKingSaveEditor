537247b4f500c4f6cda9875879fe4315018dedce
ASG <P>
2021-03-01 20:01:28 +0300

    исправил путь в open_lore_shop

--- FTK_Save_Editor.py
+++ FTK_Save_Editor.py
@@ -6,20 +6,17 @@ class SaveEditor:
 
     def __init__(self):
         self.un = os.environ.get("USERNAME")
+        self.player_db = f'C:\\Users\\{self.un}\\AppData\\LocalLow\\IronOak Games\\FTK\\save\\epic829cce0d4b4346b8989ef216f58ed67c\\player.db'
+        self.backup_player_db = f'C:\\Users\\{self.un}\\AppData\\LocalLow\\IronOak Games\\FTK\\save\\epic829cce0d4b4346b8989ef216f58ed67c\\backup_player.db'
 
     def backup(self):
-        fil = f'C:\\Users\\{self.un}\\AppData\\LocalLow\\IronOak Games\\FTK\\save\\epic829cce0d4b4346b8989ef216f58ed67c\\player.db'
-        backup_fil = f'C:\\Users\\{self.un}\\AppData\\LocalLow\\IronOak Games\\FTK\\save\\epic829cce0d4b4346b8989ef216f58ed67c\\backup_player.db'
-        with open(fil, 'rb') as file:
-            with open(backup_fil, 'wb') as backup_file:
+        with open(self.player_db, 'rb') as file:
+            with open(self.backup_player_db, 'wb') as backup_file:
                 backup_file.write(file.read())
-
-        input('backup готов')
+        print('backup готов')
 
     def update_lore(self):
-        path = f'C:\\Users\\{self.un}\\AppData\\LocalLow\\IronOak Games\\FTK\\save\\epic829cce0d4b4346b8989ef216f58ed67c\\player.db'
-
-        with sq.connect(path) as con:
+        with sq.connect(self.player_db) as con:
             cur = con.cursor()
 
             cur.execute("SELECT Value FROM sPlayerStatistic WHERE Name = 'STAT_LU_TOTAL_LORE'")
@@ -33,22 +30,20 @@ class SaveEditor:
             result = cur.fetchone()
             print(f"You have: {result[0]} LORE.")
 
-            input()
-
     def open_lore_shop(self):
-        with sq.connect(r'E:\Googl_Disc\PycharmProjects\Wazzaaap _p\SQLite3\player.db') as con:
+        with sq.connect(self.player_db) as con:
             cur = con.cursor()
             cur.execute("UPDATE sPlayerStatistic SET Value = 1 WHERE Name LIKE 'STAT_LU%'")
+        input('LORE Shop открыт')
 
 
-# ID 80 (ДАНЬ КАМЕННОМУ ГЕРОЮ) = 5
 save = SaveEditor()
-inp = input("1 - backup.save\n2 - update_morney")
+inp = input("1 - save_backup\n2 - open_lore_shop\n3 - update_LORE\n")
 if inp == '1':
     save.backup()
 elif inp == '2':
     save.open_lore_shop()
-elif inp == '2':
+elif inp == '3':
     save.update_lore()
 else:
     print('wrong chose')
