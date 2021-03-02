d4c2611b5878093755709c64a764523391242b42
ASG <P>
2021-03-02 13:49:29 +0300

    Добавления:     *Пункт 4 - give_me_money Исправления:     *В open_lore_shop добавлены исключения STAT_GOLD_WALLET и STAT_LU_TOTAL_LORE

--- FTK_Save_Editor.py
+++ FTK_Save_Editor.py
@@ -26,24 +26,41 @@ class SaveEditor:
             price = int(input('LORE = '))
             cur.execute(f"UPDATE sPlayerStatistic SET Value = {price} WHERE Name = 'STAT_LU_TOTAL_LORE'")
 
-            cur.execute('SELECT Value FROM sPlayerStatistic WHERE id = 67')
+            cur.execute("SELECT Value FROM sPlayerStatistic WHERE Name = 'STAT_LU_TOTAL_LORE'")
             result = cur.fetchone()
             print(f"You have: {result[0]} LORE.")
 
     def open_lore_shop(self):
         with sq.connect(self.player_db) as con:
             cur = con.cursor()
-            cur.execute("UPDATE sPlayerStatistic SET Value = 1 WHERE Name LIKE 'STAT_LU%'")
-        input('LORE Shop открыт')
+            cur.execute("UPDATE sPlayerStatistic SET Value = 1 WHERE Name LIKE 'STAT_LU%' AND Name != 'STAT_LU_STONE_HERO_TRIBUTES' AND Name != 'STAT_LU_TOTAL_LORE' ")
+        print('LORE Shop открыт')
+
+    def give_me_money(self):
+        with sq.connect(self.player_db) as con:
+            cur = con.cursor()
+
+            cur.execute("SELECT Value FROM sPlayerStatistic WHERE Name = 'STAT_GOLD_WALLET'")
+            result = cur.fetchone()
+            print(f"You have: {result[0]} GOLD.")
+
+            price = int(input('LORE = '))
+            cur.execute(f"UPDATE sPlayerStatistic SET Value = {price} WHERE Name = 'STAT_GOLD_WALLET'")
+
+            cur.execute("SELECT Value FROM sPlayerStatistic Name = 'STAT_GOLD_WALLET' ")
+            result = cur.fetchone()
+            print(f"You have: {result[0]} GOLD.")
 
 
 save = SaveEditor()
-inp = input("1 - save_backup\n2 - open_lore_shop\n3 - update_LORE\n")
+inp = input("1 - save_backup\n2 - open_lore_shop\n3 - update_LORE\n4 - give_me_money\n")
 if inp == '1':
     save.backup()
 elif inp == '2':
     save.open_lore_shop()
 elif inp == '3':
     save.update_lore()
+elif inp == '4':
+    save.give_me_money()
 else:
     print('wrong chose')
